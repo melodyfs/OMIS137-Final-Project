d3463e0ac8bd3b432048039383ebdd410b6ac7d2
Melody Yang <melody870116@hotmail.com>
2020-03-08 16:47:02 -0700

    Added Growth

---
+++ Growth.java
@@ -0,0 +1,21 @@
+public class Growth {
+
+    Double growthRate;
+    Double planHeight;
+    public static void main(String args[]) {
+
+    }
+
+    void weed() {
+        
+    }
+
+    Double getGrowthRate() {
+        return growthRate;
+    }
+
+    Double getPlantHeight() {
+        return growthRate;
+    }
+
+}
