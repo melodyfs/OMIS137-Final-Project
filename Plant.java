a5f9bf28bdaa6db9491f4419044b4ed6f119a606
Connor Collins <cxcollins@scu.edu>
2020-03-16 15:57:35 -0700

    updated plant class

--- Plant.java
+++ Plant.java
@@ -1,5 +1,44 @@
 public class Plant {
-    String name;
-    String species;
-    
+    protected PlantType plantType;
+    protected double startingHeight; //these are all in feet
+    protected double maxHeight;
+    protected double growthRate;
+    protected String color;
+
+    public Plant(PlantType t, double s, double m, double g, String c) {
+        plantType=t;
+        startingHeight=s;
+        maxHeight=m;
+        growthRate=g;
+        color=c;
+    }
+    PlantType getPlantType(){return plantType;}
+    double getStartingHeight() { return startingHeight; }
+    double getMaxHeight() { return maxHeight; }
+    double getGrowthRate() { return growthRate; }
+    String getColor() { return color; }
+
+    void setGrowthRate(double g){growthRate=g;}
 }
+class Rose extends Plant{
+    Rose(){super(PlantType.rose,1, 5, 0.01, "red");}}
+
+class Daisy extends Plant{
+    Daisy(){super(PlantType.daisy, 1, 2.5, 0.02, "yellow");}}
+
+class Sunflower extends Plant{
+    Sunflower(){super(PlantType.sunflower,1, 10, 0.075, "yellow");}}
+
+class Tomato extends Plant{
+    Tomato(){super(PlantType.tomato, 1, 8, 0.175, "red");}}
+
+class Basil extends Plant{
+    Basil(){super(PlantType.basil, 0.5, 2, 0.0125, "green");}}
+
+class Cucumber extends Plant{
+    Cucumber(){super(PlantType.cucumber, 0.5, 3, 0.04, "green");}}
+
+class Strawberry extends Plant{
+    Strawberry(){super(PlantType.strawberry, 0.25, 1, 0.02, "red");}}
+
+    enum PlantType{rose, daisy, sunflower, tomato, basil, cucumber, strawberry}
